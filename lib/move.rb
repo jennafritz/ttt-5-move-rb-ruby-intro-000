aed97324ffb6807072f075caff6d7a2863f2f6f5
Jenna Fritz <jennafritz24@gmail.com>
2021-02-14 23:14:24 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,5 +9,5 @@ end
 # code your input_to_index and move method here!
 def input_to_index(user_input)
   "user_input".to_i
+  converted_input = input_to_index(user_input) - 1
 end
-converted_input = input_to_index(user_input) - 1
